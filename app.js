f6424fd19088f8ca0db997e22b6a533d1fd74934
Santi Moreno <santiagom.contact@gmail.com>
2023-10-31 19:27:05 -0300

    Terminado segundo TP

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log("Servidor funcionando");
+app.listen(3001, () => {
+  console.log("Servidor funcionando en 3001");
 });
 
 app.get("/", (req, res) => {
