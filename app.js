5198335d7b7f95339a56dc7c2f1c7fdebedb2a8f
Santi Moreno <santiagom.contact@gmail.com>
2023-11-07 17:16:26 -0300

    Completada tercera entrega

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.listen(3001, () => {
   console.log("Servidor funcionando en 3001");
 });
 
+//Definimos dónde buscamos archivos js, css e imagenes
+app.use("", express.static(`${__dirname}/publics`));
+
+//definimos las distintas rutas
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
 });
